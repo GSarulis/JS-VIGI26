d582ba652dacc5a3100c9f66474fee5427383571
GSarulis <g.v.sarulis@gmail.com>
2022-06-05 21:02:27 +0300

    learning js

--- 22paskaita/app.js
+++ 22paskaita/app.js
@@ -16,17 +16,118 @@
 // });
 // console.log(promise);
 
-const rejectedPromise = new Promise((resolve, reject) => {
-  reject("Tinklas yra perkrautas.Bandykite veliau.");
+// const rejectedPromise = new Promise((resolve, reject) => {
+//   reject("Tinklas yra perkrautas.Bandykite veliau.");
+// });
+
+// rejectedPromise
+//   .then((response) => {
+//     console.log(`Dabar yra ${response}.`);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   })
+//   .finally(() => {
+//     console.log("Kreipimasis i oro tarnyba");
+//   });
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve("17 celsius, Vilnius");
+//     reject("Tinklas yra perkrautas.Bandykite veliau.");
+//   }, 4000);
+// });
+
+// const renderWeather = (response) => {
+//   const weather = document.getElementById("weather");
+//   weather.textContent = `Dabar yra ${response}.`;
+// };
+// const showErorr = (err) => {
+//   const error = document.getElementById("errors");
+//   error.textContent = err;
+//   error.style.color = "red";
+// };
+
+// const showPriceMassage = () => {
+//   const massage = document.getElementById("header");
+//   header.textContent = "Jus pasinaudojote oro paslauga. Jums tai kainavo 5ct";
+// };
+
+// const getWeather = async () => {
+//   try {
+//     const response = await promise;
+//     renderWeather(response);
+//   } catch (error) {
+//     showErorr(error);
+//   } finally {
+//     showPriceMassage();
+//   }
+// };
+
+// getWeather();
+
+// async function getWeatherFunc() {}
+
+// promise
+//   .then((response) => {
+//     // apsirasyti atvaizdavima
+//     renderWeather(response);
+//   })
+//   .catch((error) => {
+//     showErorr(error);
+//   })
+//   .finally(() => {
+//     showPriceMassage();
+//   });
+
+// namu darbai
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve();
+//   }, 1000);
+// });
+
+// promise.then(() => {
+//   console.log("Veikia");
+// });
+
+//
+// const promise2 = new Promise((resolve, reject) => {
+//   const random = Math.floor(Math.random() * 5) + 1;
+//   if (1 === random) {
+//     setTimeout(() => {
+//       reject();
+//     }, 1000);
+//   } else
+//     setTimeout(() => {
+//       resolve();
+//     }, 1000);
+// });
+
+// promise2.then(() => {
+//   console.log("Veikia");
+// });
+// promise2.catch(() => {
+//   console.log("Neveikia");
+// });
+
+//
+
+const bandymas = new Promise((resolve, reject) => {
+  const random = Math.floor(Math.random() * 5) + 1;
+  if (1 === random) {
+    setTimeout(() => {
+      reject();
+    }, 1000);
+  } else {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  }
 });
 
-rejectedPromise
-  .then((response) => {
-    console.log(`Dabar yra ${response}.`);
-  })
-  .catch((error) => {
-    console.error(error);
-  })
-  .finally(() => {
-    console.log("Kreipimasis i oro tarnyba");
-  });
+bandymas
+  .then(() => "This is message")
+  .then((message) => console.log(message))
+  .catch(() => console.log("Oops, pažadas buvo atmestas"));
